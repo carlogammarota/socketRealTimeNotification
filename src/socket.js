5e5eed99772ceb814eb62b4982703d6516854b3d
CharlyG <carlo.gammarota@gmail.com>
2022-08-25 01:52:01 -0300

    server

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
-const socket = io('localhost:3000');
+// const socket = io('localhost:3000');
+const socket = io('20.197.225.64:3000');
 // io.origins('*:*') 
 
 // socket.on("notification", (args) => {
