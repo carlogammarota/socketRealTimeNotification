ef05a7365fc2fcdca17ac5e1caca358f1de2d3c6
CharlyG <carlo.gammarota@gmail.com>
2022-09-14 06:44:41 -0300

    SERVER FOUND!

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+app.use(cors())
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    // origin: "http://localhost:8000",
+    origin: "https://yendo.site",
+    methods: ["GET", "POST"],
+    credentials: true,
   }
 });
 
